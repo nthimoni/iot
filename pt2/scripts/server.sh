e4862baaabb69dd8550e02cbe6d9791a2d09d98e
Nicolas THIMONIER <nthimoni@paul-f4Ar8s8.clusters.42paris.fr>
2025-02-17 15:05:14 +0100

    start pt2

--- pt2/scripts/server.sh
+++ pt2/scripts/server.sh
@@ -19,3 +19,14 @@ cp /var/lib/rancher/k3s/server/node-token /shared_folder/node-token
 cp /etc/rancher/k3s/k3s.yaml /shared_folder/k3s.yaml
 mkdir /home/vagrant/.kube
 cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+echo "alias k='sudo k3s kubectl'" >> /home/vagrant/.bashrc
+
+k3s kubectl create configmap app1-index --from-file=/shared_folder/app1/index.html
+k3s kubectl create configmap app2-index --from-file=/shared_folder/app2/index.html
+k3s kubectl create configmap app3-index --from-file=/shared_folder/app3/index.html
+
+k3s kubectl apply -f /shared_folder/app1/app1.yaml
+k3s kubectl apply -f /shared_folder/app2/app2.yaml
+k3s kubectl apply -f /shared_folder/app3/app3.yaml
+k3s kubectl apply -f /shared_folder/app-ingress.yaml
+
